39e36ebfa307b1f8d2497519e897c817caa8af52
Marwa Isa Jaafar Ahmed <Marwa.Mohamed@HHG.com>
2025-02-10 23:46:45 +0300

    MailboxForm Error

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,28 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-// src/App.jsx
+import { Routes, Route } from 'react-router'
+import NavBar from './component/NavBar'
+import MailboxList from './component/MailboxList'
+import MailboxForm from './component/MailboxForm'
+import MailboxDetails from './component/MailboxDetails'
 
 const App = () => {
-  return <h1>Hello world!</h1>;
+  // Create a state variable called mailboxes and initialize it with an empty array
+  const [mailboxes,setMailboxes] = useState([]);
+  return (
+
+    <>
+      <NavBar></NavBar>
+      <Routes>
+            <Route path="/" element={<main><h1>Post Office</h1></main>}></Route>
+            <Route path="/mailboxes" element={<MailboxList/>}></Route>
+            <Route path="/new-mailbox" element={<MailboxForm/>}></Route>
+            <Route path="/mailboxes/:mailboxId	" element={<MailboxDetails/>}></Route>
+
+      </Routes>
+    </>
+  )
 };
 
 export default App;
